04adbf8524d102d7bd4c06ddc04ea329d4b4aa67
krishna singh <0.krishna1120@gmail.com>
2026-08-01 09:52:34 +0530

    fix: rename password field to passwordHash in User model; update RefreshToken model to use hashedToken

---
+++ apps/backend/src/auto/auth.routes.ts
@@ -0,0 +1,10 @@
+// import { Router } from "express";
+// import { validate } from "../shared/middlewares/validate.middleware.js";
+// import controller from "./auth.controller.js";
+// import { loginSchema, registerSchema } from "./auth.schema.js";
+// import router from "../routes/index.js";
+
+// router.post("/register", validate(registerSchema), controller.register);
+// router.post("/login", validate(loginSchema), controller.login);
+// router.post("/refresh", controller.refresh);
+// router.post("/logout", controller.logout);
